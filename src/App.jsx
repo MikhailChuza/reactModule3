2f40d7156db5096c9881745b32019c7f52de5be4
MikhailChuza <148324632+MikhailChuza@users.noreply.github.com>
2024-05-08 15:29:01 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,106 @@
+import LoginForm from "./LoginForm/LoginForm.jsx";
+// import LangSwitcher from "./LangSwitcher/LangSwitcher.jsx";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+// const App = () => {
+//   // Колбек-функція для обробки сабміту форми
+//   const handleLogin = (userData) => {
+//     // Виконуємо необхідні операції з даними
+//     console.log(userData);
+//   };
+
+//   return (
+//     <div>
+//       <h1>Please login to your account!</h1>
+//       {/* Передаємо колбек як пропс форми */}
+//       <LoginForm onLogin={handleLogin} />
+//     </div>
+//   );
+// };
+
+// export default function App() {
+//   const [lang, setLang] = useState("uk");
+
+//   return (
+//     <>
+//       <p>Selected language: {lang}</p>
+//       <LangSwitcher value={lang} onSelect={setLang} />
+//     </>
+//   );
+// }
+
+// export default function App() {
+//   const [coffeeSize, setCoffeeSize] = useState("sm");
+
+//   const handleSizeChange = (evt) => {
+//     setCoffeeSize(evt.target.value);
+//   };
+
+//   return (
+//     <>
+//       <h1>Select coffee size</h1>
+//       <div className="options">
+//         <label>
+//           <input
+//             type="radio"
+//             name="coffeeSize"
+//             value="sm"
+//             checked={coffeeSize === "sm"}
+//             onChange={handleSizeChange}
+//           />
+//           Small
+//         </label>
+//         <label>
+//           <input
+//             type="radio"
+//             name="coffeeSize"
+//             value="md"
+//             checked={coffeeSize === "md"}
+//             onChange={handleSizeChange}
+//           />
+//           Meduim
+//         </label>
+//         <label>
+//           <input
+//             type="radio"
+//             name="coffeeSize"
+//             value="lg"
+//             checked={coffeeSize === "lg"}
+//             onChange={handleSizeChange}
+//           />
+//           Large
+//         </label>
+//       </div>
+//       <p>
+//         <b>Selected size:</b> {coffeeSize}
+//       </p>
+//     </>
+//   );
+// }
+const App = () => {
+  const [hasAccepted, setHasAccepted] = useState(false);
+
+  const handleChange = (evt) => {
+    setHasAccepted(evt.target.checked);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          name="terms"
+          checked={hasAccepted}
+          onChange={handleChange}
+        />
+        I accept terms and conditions
+      </label>
+      <button type="button" disabled={!hasAccepted}>
+        Proceed
+      </button>
+    </div>
   );
-}
+};
 
 export default App;
